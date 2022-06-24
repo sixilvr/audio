affbf4ccac17fd2f4460c123d0e31d7b8b63c7a5
unknown <shahirlatif1@gmail.com>
2022-06-24 10:38:25 -0500

    Default range to full song for repeat_pattern

--- arrangement.py
+++ arrangement.py
@@ -6,7 +6,7 @@ from . import utils
 
 class Arrangement(Pattern):
     def __init__(self, bpm, num_measures, beats_per_measure = 4):
-        super().__init__(bpm, num_measures * beats_per_measure, 44100)
+        super().__init__(bpm, num_measures * beats_per_measure)
         self.beats_per_measure = beats_per_measure
 
     def __repr__(self):
@@ -16,12 +16,14 @@ class Arrangement(Pattern):
     def measures(self):
         return utils.samples_to_beats(self.bpm, len(self), self.samplerate) / self.beats_per_measure
 
-    def place_pattern(self, pattern, measure_location, num_beats = None):
+    def place_pattern(self, pattern, measure_location = 1, num_beats = None):
         if num_beats is None:
             self.place(pattern, (measure_location - 1) * self.beats_per_measure + 1)
         else:
             self.place(pattern.sub_pattern(1, num_beats), (measure_location - 1) * self.beats_per_measure + 1)
 
-    def repeat_pattern(self, pattern, start_measure, end_measure, multiplier = 1):
+    def repeat_pattern(self, pattern, start_measure = 1, end_measure = -1, multiplier = 1):
+        if end_measure == -1:
+            end_measure = self.measures
         repetitions = int((end_measure - start_measure + 1) / pattern.beats * self.beats_per_measure)
         self.roll(pattern, (start_measure - 1) * self.beats_per_measure + 1, repetitions, pattern.beats, multiplier)
