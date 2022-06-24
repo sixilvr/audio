b44f7673488ff69baaeaddd1c179603c95e280a9
unknown <shahirlatif1@gmail.com>
2022-06-24 13:13:31 -0500

    Use patterns instead of measures; allow arrangement notation

--- arrangement.py
+++ arrangement.py
@@ -1,29 +1,32 @@
-import numpy as np
-
-from . import Sound
 from . import Pattern
-from . import utils
 
 class Arrangement(Pattern):
-    def __init__(self, bpm, num_measures, beats_per_measure = 4):
-        super().__init__(bpm, num_measures * beats_per_measure)
-        self.beats_per_measure = beats_per_measure
+    def __init__(self, bpm, num_patterns, beats_per_pattern = 8):
+        super().__init__(bpm, num_patterns * beats_per_pattern)
+        self.beats_per_pattern = beats_per_pattern
 
     def __repr__(self):
-        return f"audio.Arrangement(bpm = {self.bpm}, num_measures = {self.num_measures}, beats_per_measure = {self.beats_per_measure}, samplerate = {self.samplerate})"
+        return f"audio.Arrangement(bpm = {self.bpm}, num_patterns = {self.num_patterns}, beats_per_pattern = {self.beats_per_pattern})"
 
     @property
-    def measures(self):
-        return utils.samples_to_beats(self.bpm, len(self), self.samplerate) / self.beats_per_measure
+    def num_patterns(self):
+        return self.beats / self.beats_per_pattern
 
-    def place_pattern(self, pattern, measure_location = 1, num_beats = None):
+    def place_pattern(self, pattern, pattern_location = 1, num_beats = None):
         if num_beats is None:
-            self.place(pattern, (measure_location - 1) * self.beats_per_measure + 1)
+            self.place(pattern, (pattern_location - 1) * self.beats_per_pattern + 1)
         else:
-            self.place(pattern.sub_pattern(1, num_beats), (measure_location - 1) * self.beats_per_measure + 1)
+            self.place(pattern.sub_pattern(1, num_beats), (pattern_location - 1) * self.beats_per_pattern + 1)
 
-    def repeat_pattern(self, pattern, start_measure = 1, end_measure = -1, multiplier = 1):
-        if end_measure == -1:
-            end_measure = self.measures
-        repetitions = int((end_measure - start_measure + 1) / pattern.beats * self.beats_per_measure)
-        self.roll(pattern, (start_measure - 1) * self.beats_per_measure + 1, repetitions, pattern.beats, multiplier)
+    def repeat_pattern(self, pattern, start_location = 1, end_location = -1, multiplier = 1):
+        if end_location == -1:
+            end_location = self.num_patterns
+        repetitions = int((end_location - start_location + 1) * self.beats_per_pattern / pattern.beats)
+        self.roll(pattern, (start_location - 1) * self.beats_per_pattern + 1, repetitions, pattern.beats, multiplier)
+
+    def arrange_pattern(self, pattern, arrangement_pattern, hit_char = "1"):
+        if len(arrangement_pattern) != self.num_patterns:
+            raise ValueError(f"Invalid arrangement pattern length: expected {self.num_patterns}, but got {len(arrangement_pattern)}")
+        for i, char in enumerate(arrangement_pattern):
+            if char == hit_char:
+                self.place_pattern(pattern, i + 1)
